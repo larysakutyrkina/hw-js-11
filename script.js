f6d190b89bba56d69f93ff8775ba78e4db76697a
larysakutyrkina <kutyrkina.larysa@gmail.com>
2021-02-10 23:24:32 +0200

    ...

---
+++ script.js
@@ -0,0 +1,11 @@
+let button = document.querySelectorAll(".btn"); 
+ 
+document.addEventListener("keydown", function (e) { 
+    button.forEach((item) => { 
+        if (e.key.toUpperCase() === item.dataset.name.toUpperCase()) { 
+            item.classList.add("color"); 
+        } else { 
+            item.classList.remove("color"); 
+        } 
+    }); 
+});
